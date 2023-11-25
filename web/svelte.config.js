3499f4a58c0799091d129cd48650b4855847c55b
johannes-adr <john.rans@web.de>
2023-11-25 17:05:31 +0100

    Added web

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,14 +12,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html',
-			
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : "/ComputerGrafik"
-		}
-	},
+		adapter: adapter()
+	}
 };
 
 export default config;
