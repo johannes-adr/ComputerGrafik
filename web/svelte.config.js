e91120b20c756fcb6f9e28e3929d9912e6363adc
johannes-adr <john.rans@web.de>
2023-11-25 16:49:24 +0100

    Added web

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -12,12 +12,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: 'index.html',
+			
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : "/ComputerGrafik"
 		}
-	}
+	},
 };
 
 export default config;
